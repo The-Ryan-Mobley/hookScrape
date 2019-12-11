f94f85366027b62c2ac8ea0aba709b51daf2d10a
Ryan Mobley <ryan.mobley94@gmail.com>
2019-12-10 18:48:32 -0600

    building backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const PORT = process.env.PORT || 1337;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rocksFall");
+mongoose.set('useFindAndModify', false);
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
